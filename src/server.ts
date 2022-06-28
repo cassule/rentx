a01dbf3b350c2d35dbe8fa90be9e737b1ae89b6e
Josué Francisco <josuefrancisco2017@outlook.pt>
2022-06-28 17:30:20 +0100

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+
+import express from "express";
+import swaggerUI from "swagger-ui-express";
+
+import { createConnection } from "./database/data-source";
+import { router } from "./routes";
+import swaggerFile from "./swagger.json";
+
+createConnection();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+app.use(router);
+
+app.listen(3333, () => {
+  console.log("Server is running on port 3333");
+});
